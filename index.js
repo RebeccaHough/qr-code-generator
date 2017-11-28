47eb676a4afa4e54cfc7c779aaab739d377728a2
Rebecca Hough <rebecca.hough@flo-group.com>
2017-11-28 18:32:15 +0800

    update: switched expected input file format to JSON (formerly csv), added checks against schema, added output dir

--- index.js
+++ index.js
@@ -1,72 +1,52 @@
 var qrCodeGenerator = require('qrcode')
 var fs = require('fs');
 
-//expect file to look like:
-//id, 1, 
-//name, a, 
-//type, typeA; //<---- qr code separator
-//id, 2, 
-//name, b, 
-//type, typeB
+//the test file, 'input.json', shows the expected format
 
-
-fs.readFile('input.csv', 'utf8', function(err, input){
+fs.readFile('input.json', 'utf8', function(err, input){
     if (err) throw err;
     //DEBUG console.log(input);
 
-    //split input into one JSON object per qr code
-    var qrCodeArr = [], qrCodeCount = 0, nextChar;
-    //for each char in input
-    for(var charCount = 0; charCount < input.length; charCount++) {
-        nextChar = input.charAt(charCount);
-        //if new qr code
-        if(nextChar == ';') {
-            qrCodeArr[qrCodeCount] += "\" }"; //close current object
-            qrCodeCount++; //create new array element
-            charCount++; //skip newline
-        } else {
-            //replace newlines with commas
-            if(nextChar != '\n') {
-                //replace commas with colons
-                if(nextChar != ',') {
-                    //if array element is unintialised
-                    if(qrCodeArr[qrCodeCount] == undefined) {
-                        //start new JSON object
-                        qrCodeArr[qrCodeCount] = "{ \"" + nextChar;
-                    } else {
-                        //add next char to current JSON object
-                        qrCodeArr[qrCodeCount] += nextChar;
-                    }
-                } else {
-                    qrCodeArr[qrCodeCount] += "\": \"";
-                }
-            } else {
-                qrCodeArr[qrCodeCount] += "\" , \"";
-            }
-        } 
-    }
-    //close final JSON object (since input file is not expected to end in a semicolon)
-    qrCodeArr[qrCodeCount] += "\" }";
-
     //error checking
-    //DEBUG console.log(qrCodeArr);
-    if(qrCodeArr == undefined) throw "file contents not in expected format";
-    for(var i = 0; i < qrCodeArr.length; i++) {
-        try {
-            //DEBUG console.log(qrCodeArr[i]);
-            var test = JSON.parse(qrCodeArr[i]);
-            //DEBUG console.log(test);
-        } catch(e) {
-            throw "cannot convert file contents to valid JSON";
-        }
+
+    //check input is valid json
+    try {
+        //(if successful, keep cast to javascript object for easier processing)
+        input = JSON.parse(input);
+    } catch(e) {
+        throw "Input file is not valid JSON";
+    }
+    //check input follows schema
+    //check hits field exists
+    if(!input.hasOwnProperty('hits')) throw "Invalid JSON format: no property 'hits' found"
+    //check hits field has a field hits
+    if(!input.hits.hasOwnProperty('hits')) throw "Invalid JSON format: no property 'hits' found on 'hits'"
+    //check this hits field is an array
+    if(!(input.hits.hits instanceof Array)) throw "Invalid JSON format: property 'hits' on 'hits' is not an array"
+    //check that the hits array is not empty
+    if(input.hits.hits.length < 1) throw "Invalid JSON format: property 'hits' on 'hits' is an empty array"
+    for(var i = 0; i < input.hits.hits.length; i++) {
+        //check that every member of the input.hits.hits array has an element '_source'
+        if(!input.hits.hits[i].hasOwnProperty('_source')) throw "Invalid JSON format: one or more members of 'hits' array does not have an element '_source'"
+        //check that _source array is not empty
+        if(input.hits.hits[i]._source.length < 1) throw "Invalid JSON format: property '_source' on 'hits' is empty"
+        //check that _source has all required propeties
+        if(!input.hits.hits[i]._source.hasOwnProperty('guid') || !input.hits.hits[i]._source.hasOwnProperty('aliases') || !input.hits.hits[i]._source.hasOwnProperty('carton_size')) throw "Invalid JSON format: one or more properties '_source' on 'hits' does not have all properties required by schema"
     }
 
-    //convert output to qr code and download
+    //if input.json passes all checks, generate QR codes
+
+    //extract 'hits' array
+    var hits = input.hits.hits;
+    //for each javascript obj in hits
     var i;
-    for(i = 0; i < qrCodeArr.length; i++) {
+    for(i = 0; i < hits.length; i++) {
+        //cast back to json
+        hits[i] = JSON.stringify(hits[i]._source);
+        //DEBUG console.log(hits[i]);
+        //create qr code
         var date = new Date().valueOf();
-        console.log(date);
-        qrCodeGenerator.toFile("qr code created " + date + ".png", qrCodeArr[i], function(err) {
+        qrCodeGenerator.toFile("./output/qr-code-created-" + date + ".png", hits[i], function(err) {
             if (err) throw err;
         });
     }

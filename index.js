bdc572602eccad3d20ef847e511b8b8f9a3c8238
Rebecca Hough <rebecca.hough@flo-group.com>
2017-12-01 17:28:14 +0800

    update: edited script to use command line arguments to get input - can accept file paths or raw JSON enclosed by single quotes.

--- index.js
+++ index.js
@@ -1,22 +1,60 @@
 var qrCodeGenerator = require('qrcode')
 var fs = require('fs');
 
-//the test file, 'input.json', shows the expected format
+//get command line arguments
+input = process.argv[2];
 
-fs.readFile('input.json', 'utf8', function(err, input){
-    if (err) throw err;
-    //DEBUG console.log(input);
+//test whether input was a path to a file or a JSON string
+validatedInput = validateJSON(input);
+//if validation is unsuccesful, the original input will be returned hence the below means: if(input_is_not_JSON)
+if(validatedInput === input) {
+    //assume input is a file and attempt to open it
+    fs.readFile(input, 'utf8', function(err, input) {
+        //if file cannot be opened
+        if (err) {
+            console.log("Could not read file. If input was intended to be raw JSON, please ensure that it is surrounded by single quote marks (').");
+            throw err;
+        }
+        console.log("File opened.");
 
-    //error checking
+        //check input is valid json
+        validatedInput = validateJSON(input);
+        //if validation is unsuccesful, the original input will be returned hence the below means: if(input_is_not_JSON)
+        if(validatedInput === input) {
+            throw "Input file does not contain valid JSON";
+        } else {
+            verifyJSON(validatedInput);
+            //if checks succeed, create qr codes
+            generateQrCodes(validatedInput);
+        }
+    }); 
+} else {
+    //else command line arg was valid JSON
+    //so check it against schema
+    verifyJSON(validatedInput);
+    //if checks succeed, create qr codes
+    generateQrCodes(validatedInput);
+}
 
-    //check input is valid json
+/** 
+ * Check whether the input string is a valid JSON object, then cast it to a JavaScript object if possible 
+ * @param {string} input - input to be validated
+ * @return input as a JavaScript object if the cast from JSON was possible, or the orginal input if the cast was not possible
+ */
+function validateJSON(input) {
     try {
-        //(if successful, keep cast to javascript object for easier processing)
         input = JSON.parse(input);
+        return input;
     } catch(e) {
-        throw "Input file is not valid JSON";
+        return input;
     }
-    //check input follows schema
+}
+
+/**
+ * Check input follows the schema, and halt execution if deviation from schema is present
+ * @param {Object} input - input to be verified
+ */
+function verifyJSON(input) {
     //check hits field exists
     if(!input.hasOwnProperty('hits')) throw "Invalid JSON format: no property 'hits' found"
     //check hits field has a field hits
@@ -33,15 +71,19 @@ fs.readFile('input.json', 'utf8', function(err, input){
         //check that _source has all required propeties
         if(!input.hits.hits[i]._source.hasOwnProperty('guid') || !input.hits.hits[i]._source.hasOwnProperty('aliases') || !input.hits.hits[i]._source.hasOwnProperty('carton_size')) throw "Invalid JSON format: one or more properties '_source' on 'hits' does not have all properties required by schema"
     }
+}
 
-    //if input.json passes all checks, generate QR codes
-
+/**
+ * For each member of the input.hits.hits array, create a QR code PNG of its '_source' property and store it in an output folder
+ * @param {Object} input - input to be converted to QR codes. Must have a property names 'hits' which has an array named 'hits' as a property, each member of which must have a property named '_source'
+ */
+function generateQrCodes(input) {
     //extract 'hits' array
     var hits = input.hits.hits;
-    //for each javascript obj in hits
+    //for each JavaScript object in hits
     var i;
     for(i = 0; i < hits.length; i++) {
-        //cast back to json
+        //cast back to JSON and reduce amount of code needed to access _source objects
         hits[i] = JSON.stringify(hits[i]._source);
         //DEBUG console.log(hits[i]);
         //create qr code
@@ -51,5 +93,4 @@ fs.readFile('input.json', 'utf8', function(err, input){
         });
     }
     console.log("Successfully created (" + i + ") files.");
-}) 
-
+}
